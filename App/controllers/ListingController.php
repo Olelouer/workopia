d443f7798cc9f55b3275f87c6062e38c6817b4a7
Olelouer <olivier.lelouer@protonmail.com>
2025-03-06 21:08:25 +0100

    add crud

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use Framework\Database;
+use Framework\Validation;
 
 class ListingController
 {
@@ -37,13 +38,14 @@ class ListingController
     }
 
     /**
-     * Create a single listing
+     * Show a single listing
      * 
+     * @param array $params
      * @return void
      */
-    public function show($param)
+    public function show($params)
     {
-        $id = $param['id'] ?? '';
+        $id = $params['id'] ?? '';
 
         $params = [
             'id' => $id
@@ -60,5 +62,192 @@ class ListingController
         loadView('listings/show', [
             'listing' => $listing
         ]);
+    }
+
+    /**
+     * Store data in database
+     * 
+     * @return void
+     */
+    public function store()
+    {
+        $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
+
+        // array_intersect_key, check if keys are similar, array_flip, turn values into keys and keys into values
+        $newListingData = array_intersect_key($_POST, array_flip($allowedFields));
+
+        $newListingData['user_id'] = 1;
+
+        $newListingData = array_map('sanitize', $newListingData);
+
+        $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state'];
+
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if (empty($newListingData[$field]) || !Validation::string($newListingData[$field])) {
+                $errors[$field] = ucfirst($field) . ' is required';
+            }
+        }
+
+        if (!empty($errors)) {
+            // Reload view with errors
+            loadView('listing/create', [
+                'errors' => $errors,
+                'listing' => $newListingData
+            ]);
+        } else {
+            // Submit data
+            $fields = [];
+
+            foreach ($newListingData as $field => $value) {
+                $fields[] = $field;
+            }
+
+            $fields = implode(', ', $fields);
+
+            $values = [];
+
+            foreach ($newListingData as $field => $value) {
+                if ($value === '') {
+                    $newListingData[$field] = null;
+                }
+
+                $values[] = ':' . $field;
+            }
+
+            $values = implode(', ', $values);
+
+            $query = "INSERT INTO listings ({$fields}) VALUES ({$values})";
+
+            $this->db->query($query, $newListingData);
+
+            redirect('/listings');
+        }
+    }
+
+    /**
+     * Delete a listing
+     * 
+     * @param array $params
+     * 
+     * @return void
+     */
+    public function destroy($params)
+    {
+        $id = $params['id'];
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        $this->db->query('DELETE FROM listings WHERE id = :id', $params);
+
+        // Set flash message
+        $_SESSION['success_message'] = 'Listing deleted successfully';
+
+        redirect('/listings');
+    }
+
+    /**
+     * Show the listing edit form
+     * 
+     * @param array $params
+     * @return void
+     */
+    public function edit($params)
+    {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        // Check if listing exist
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        loadView('listings/edit', [
+            'listing' => $listing
+        ]);
+    }
+
+    /**
+     * Update a listing
+     * 
+     * @param array $params
+     * 
+     * @return void
+     */
+    public function update($params)
+    {
+        $id = $params['id'] ?? '';
+
+        $params = [
+            'id' => $id
+        ];
+
+        $listing = $this->db->query('SELECT * FROM listings WHERE id = :id', $params)->fetch();
+
+        // Check if listing exist
+        if (!$listing) {
+            ErrorController::notFound('Listing not found');
+            return;
+        }
+
+        $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
+
+        $updateValues = [];
+
+        $updateValues = array_intersect_key($_POST, array_flip($allowedFields));
+
+        $updateValues = array_map('sanitize', $updateValues);
+
+        $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state'];
+
+        $errors = [];
+
+        foreach ($requiredFields as $field) {
+            if (empty($updateValues[$field]) || !Validation::string($updateValues[$field])) {
+                $errors[$field] = ucfirst($field) . ' is required';
+            }
+        }
+
+        if (!empty($errors)) {
+            loadView('listings/edit', [
+                'listing' => $listing,
+                'errors' => $errors
+            ]);
+            exit;
+        } else {
+            // Submit to database
+            $updateFields = [];
+
+            foreach (array_keys($updateValues) as $field) {
+                $updateFields[] = "{$field} = :{$field}";
+            }
+
+            $updateFields = implode(', ', $updateFields);
+
+            $updateValues['id'] = $id;
+            $updateQuery = "UPDATE listings SET $updateFields WHERE id = :id";
+
+            $this->db->query($updateQuery, $updateValues);
+
+            $_SESSION['success_message'] = 'Listing Updated';
+
+            redirect('/listings/' . $id);
+        }
     }
 }
--- App/views/listings/edit.view.php
+++ App/views/listings/edit.view.php
@@ -7,8 +7,9 @@ loadPartial('top-banner');
 <!-- Post a Job Form Box -->
 <section class="flex justify-center items-center mt-20">
     <div class="bg-white p-8 rounded-lg shadow-md w-full md:w-600 mx-6">
-        <h2 class="text-4xl text-center font-bold mb-4">Create Job Listing</h2>
-        <form method="POST" action="/listings">
+        <h2 class="text-4xl text-center font-bold mb-4">Edit Job Listing</h2>
+        <form method="POST" action="/listings/<?= $listing->id ?>">
+            <input type="hidden" name="_method" value="PUT">
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                 Job Info
             </h2>
@@ -23,13 +24,13 @@ loadPartial('top-banner');
                     name="title"
                     placeholder="Job Title"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['title'] ?? '' ?>" />
+                    value="<?= $listing->title ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <textarea
                     name="description"
                     placeholder="Job Description"
-                    class="w-full px-4 py-2 border rounded focus:outline-none"><?= $listing['description'] ?? '' ?></textarea>
+                    class="w-full px-4 py-2 border rounded focus:outline-none"><?= $listing->description ?? '' ?></textarea>
             </div>
             <div class="mb-4">
                 <input
@@ -37,7 +38,7 @@ loadPartial('top-banner');
                     name="salary"
                     placeholder="Annual Salary"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['salary'] ?? '' ?>" />
+                    value="<?= $listing->salary ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input
@@ -45,7 +46,7 @@ loadPartial('top-banner');
                     name="requirements"
                     placeholder="Requirements"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['requirements']  ?? '' ?>" />
+                    value="<?= $listing->requirements  ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input
@@ -53,7 +54,7 @@ loadPartial('top-banner');
                     name="benefits"
                     placeholder="Benefits"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['benefits'] ?? '' ?>" />
+                    value="<?= $listing->benefits ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input
@@ -61,7 +62,7 @@ loadPartial('top-banner');
                     name="tags"
                     placeholder="Tags"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['tags'] ?? '' ?>" />
+                    value="<?= $listing->tags ?? '' ?>" />
             </div>
             <h2 class="text-2xl font-bold mb-6 text-center text-gray-500">
                 Company Info & Location
@@ -72,7 +73,7 @@ loadPartial('top-banner');
                     name="company"
                     placeholder="Company Name"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['company'] ?? '' ?>" />
+                    value="<?= $listing->company ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input
@@ -80,7 +81,7 @@ loadPartial('top-banner');
                     name="address"
                     placeholder="Address"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['address'] ?? '' ?>" />
+                    value="<?= $listing->address ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input
@@ -88,7 +89,7 @@ loadPartial('top-banner');
                     name="city"
                     placeholder="City"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['city'] ?? '' ?>" />
+                    value="<?= $listing->city ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input
@@ -96,7 +97,7 @@ loadPartial('top-banner');
                     name="state"
                     placeholder="State"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['state'] ?? '' ?>" />
+                    value="<?= $listing->state ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input
@@ -104,7 +105,7 @@ loadPartial('top-banner');
                     name="phone"
                     placeholder="Phone"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['phone'] ?? '' ?>" />
+                    value="<?= $listing->phone ?? '' ?>" />
             </div>
             <div class="mb-4">
                 <input
@@ -112,14 +113,14 @@ loadPartial('top-banner');
                     name="email"
                     placeholder="Email Address For Applications"
                     class="w-full px-4 py-2 border rounded focus:outline-none"
-                    value="<?= $listing['email'] ?? '' ?>" />
+                    value="<?= $listing->email ?? '' ?>" />
             </div>
             <button
                 class="w-full bg-green-500 hover:bg-green-600 text-white px-4 py-2 my-3 rounded focus:outline-none">
                 Save
             </button>
             <a
-                href="/"
+                href="/listings/<?= $listing->id ?>"
                 class="block text-center w-full bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded focus:outline-none">
                 Cancel
             </a>
